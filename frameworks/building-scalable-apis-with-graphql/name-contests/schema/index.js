295de64bdb5ffaddcb15135b872b0188c974fbcd
Eric Helander <eric.p.helander@gmail.com>
2020-04-27 13:25:07 -0500

    Add name-contests, not as submodule

---
+++ frameworks/building-scalable-apis-with-graphql/name-contests/schema/index.js
@@ -0,0 +1,19 @@
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const RootQueryType = new GraphQLObjectType({
+  name: "RootQueryType",
+
+  fields: {
+    hello: {
+      type: GraphQLString,
+      description: "The *mandatory* hello world example. GraphQL style.",
+      resolve: () => "world",
+    },
+  },
+});
+
+const ncSchema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+module.exports = ncSchema;
