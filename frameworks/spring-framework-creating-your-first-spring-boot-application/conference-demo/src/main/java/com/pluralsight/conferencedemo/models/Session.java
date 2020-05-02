871d64e252c860caaf575e2f08846e6e883a21ac
Eric Helander <eric.p.helander@gmail.com>
2020-05-01 21:10:36 -0500

    Completed: spring framework creating your first spring boot application: building apps with spring boot: demo creating jpa models

--- frameworks/spring-framework-creating-your-first-spring-boot-application/conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ frameworks/spring-framework-creating-your-first-spring-boot-application/conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -5,11 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name="sessions")
+@Entity(name = "sessions")
 public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long session_id;
+
     private String session_name;
     private String session_description;
     private Integer session_length;
